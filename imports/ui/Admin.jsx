e3cde5d4312b33dd479e1f595f480155318a2475
Matt Charters <matthew@thirdsectorpublishing.ca>
2017-04-10 00:22:41 -0400

    Show all teams in admin panel

--- imports/ui/Admin.jsx
+++ imports/ui/Admin.jsx
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
+import Players from '../api/players.js';
 
-const Admin = ({ users }) => (
+const Admin = ({ users, teams }) => (
   <div className="container">
     <h1>Admin</h1>
     <button
@@ -30,7 +31,21 @@ const Admin = ({ users }) => (
           ))}
         </tr>
       </thead>
-      <tbody />
+      <tbody>
+        <tr>
+          {users && users.map(user => (
+            <td key={user._id}>
+              <ol>
+                {teams[user._id].map(player => (
+                  <li key={player._id}>
+                    {player.playerName} ({player.playerTeamsPlayedFor})
+                  </li>
+                ))}
+              </ol>
+            </td>
+          ))}
+        </tr>
+      </tbody>
     </table>
   </div>
 );
@@ -43,6 +58,15 @@ Admin.defaultProps = {
   users: [],
 };
 
-export default createContainer(() => ({
-  users: Meteor.users.find().fetch(),
-}), Admin);
+export default createContainer(() => {
+  const users = Meteor.users.find().fetch();
+  const teams = {};
+  users.forEach((user) => {
+    teams[user._id] = Players.find({ userId: user._id }, { sort: { selected: 1 } });
+  });
+
+  return {
+    users,
+    teams,
+  };
+}, Admin);
